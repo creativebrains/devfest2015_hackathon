2bb0077aa32a125ab0ec0e886cbb30f0f91d1b3b
Dominik Kukacka <dominik.kukacka@gmail.com>
2015-11-22 14:37:12 +0100

    asdf

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -56,6 +56,46 @@ angular.module('starter.controllers', [])
     // $scope.seesBeacon = BeaconService.isInRange('F0:66:6A:5E:49:6B'); // clemens
     // $scope.seesBeacon = BeaconService.isInRange('CA:FB:74:4E:C9:24'); // markus
     $scope.seesBeacon = BeaconService.isInRange('D0:49:15:04:0F:14'); // dominik
+    $scope.beacon = BeaconService.get('D0:49:15:04:0F:14');
 
   }, 1000);
-});
+})
+
+.controller('WeekController', function($scope) {
+
+  $scope.weekDays = [
+    {
+      date: moment('2015-11-16').format('dddd - DD.MM.YYYY'),
+      hours: 10.5
+    },
+    {
+      date:  moment('2015-11-16').add(1, 'day').format('dddd - DD.MM.YYYY'),
+      hours: 0.0
+    },
+    {
+      date:  moment('2015-11-16').add(2, 'day').format('dddd - DD.MM.YYYY'),
+      hours: 8.0
+    },
+    {
+      date:  moment('2015-11-16').add(3, 'day').format('dddd - DD.MM.YYYY'),
+      hours: 10.5
+    },
+    {
+      date:  moment('2015-11-16').add(4, 'day').format('dddd - DD.MM.YYYY'),
+      hours: 3
+    }
+  ];
+
+
+  var Checkin = Parse.Object.extend('CheckIn');
+  var query = new Parse.Query(Checkin);
+  query.equalTo('user', Parse.User.current());
+  query.find({
+    success: function(results) {
+      console.log('results',JSON.stringify(results));
+    },
+    error: function(error) {
+      alert('Error: ' + error.code + ' ' + error.message);
+    }
+  });
+})
